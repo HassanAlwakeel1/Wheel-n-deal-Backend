1d8b6ed0dbf00db0eb69950e1b6f0346c4a025c2
Nouran Hosameldin <nouran_31070288@ics.tanta.edu.eg>
2024-12-14 03:42:32 +0200

    add Redis Configuration

--- GraduationProject/src/main/java/com/graduationproject/GraduationProject.java
+++ GraduationProject/src/main/java/com/graduationproject/GraduationProject.java
@@ -13,10 +13,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //http://localhost:2000/swagger-ui.html
 
+@EnableCaching
 @SpringBootApplication
 @EnableConfigurationProperties
 @OpenAPIDefinition(info = @Info(title = "Wheel n' Deal", version = "1.0", description = "testing level"))
